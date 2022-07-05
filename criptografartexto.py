78640fb4aa00375fd72125ae85dfffdc6cbb88fd
rdgmrs <rdrgmrsdslv@gmail.com>
2022-07-05 16:31:24 -0300

    aa

--- criptografartexto.py
+++ criptografartexto.py
@@ -5,20 +5,58 @@
 
 # janela = tkinter.Tk()
 # janela.title("NutryNotepad")
-# janela.mainloop()
+# janela.mainloop() 
 # ------------------------------
 # ------------------------------
-nomedoaquivo = input(str)
-print(nomedoaquivo)
+# nomedoaquivo = input(str)
+# print(nomedoaquivo)
 
-test_str = 'ONIBUS'
-print('a palavra é : '+str(test_str))
-res = ''.join(format(ord(i), 'b')for i in test_str)
-print('convertido para binário : '+ str(res))
+#### de srt pra binario####
+# test_str = "GeeksforGeeks"
+# print("The original string is : " + str(test_str)) 
+# res = ''.join(format(i, 'b') for i in bytearray(test_str, encoding ='utf-8')) 
+# print("The string after binary conversion : " + str(res)) 
 
-# nomedoaquivo = ('utf-8' b'nomedoarquivo'.encode)
-# arqbinnome = input(int)
-# print(arqbinnome)
+####de bin pra srt#####
+# def BinaryToDecimal(binary):  
+         
+#     binary1 = binary  
+#     decimal, i, n = 0, 0, 0
+#     while(binary != 0):  
+#         dec = binary % 10
+#         decimal = decimal + dec * pow(2, i)  
+#         binary = binary//10
+#         i += 1
+#     return (decimal)     
+# bin_data ='10001111100101110010111010111110011'
+   
+# print("The binary value is:", bin_data) 
+   
+# str_data =' '
+   
+# for i in range(0, len(bin_data), 7): 
+      
+    
+    
+#     temp_data = int(bin_data[i:i + 7]) 
+       
+    
+    
+#     decimal_data = BinaryToDecimal(temp_data) 
+       
+    
+    
+    
+    
+    
+#     str_data = str_data + chr(decimal_data)  
+   
+# print("The Binary value after string conversion is:",  
+#        str_data)
+
+
+
+
 #cria um arquivo txt e escreve nele uma vez
 # def novotxt(): 
 #     with open(nomedoaquivo, 'w+') as file: #escreve/cria o txt
